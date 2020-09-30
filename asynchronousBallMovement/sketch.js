36eddfac79cf4c1b7d4591c7bfccf4ef8424b808
Nk1672 <69999505+Nk1672@users.noreply.github.com>
2020-09-29 19:55:46 -0700

    Add files via upload

---
+++ asynchronousBallMovement/sketch.js
@@ -0,0 +1,20 @@
+var canvas,backgroundImage, gameState=0, playerCount;
+var database, form, player,game;
+var allPlayers,distance=0;
+
+function setup(){
+    canvas=createCanvas(400,400);
+    database=firebase.database();
+    game=new Game();
+    game.getState();
+    game.start();
+}
+function draw(){
+    if(playerCount==4){
+        game.update(1);
+    }
+    if(gameState==1){
+        clear();
+        game.play()
+    }
+}
